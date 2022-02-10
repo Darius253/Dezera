2eb7e43d51d63ceab0db2020e33ab85b8c72fecf
Darius <68332672+Darius253@users.noreply.github.com>
2022-02-10 23:33:53 +0000

    Addeds few details

--- dezera/lib/views/additionalinformation.dart
+++ dezera/lib/views/additionalinformation.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
-Widget additionalInformation(String wind, String pressure, String humidity, String feels_like) {
+TextStyle title = const TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
+TextStyle info = const TextStyle(fontWeight: FontWeight.w400, fontSize: 18);
+
+
+Widget additionalInformation(
+    String wind, String pressure, String humidity, String feelslike) {
   return Container(
     width: double.infinity,
     padding: const EdgeInsets.all(18.0),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
-
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-             Column(
-               mainAxisAlignment: MainAxisAlignment.start,
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text(wind, style: const TextStyle(fontWeight: FontWeight.w600),)
-               ],
-             )
-          ]
-        )
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Wind", style: title),
+                  const SizedBox(height: 18),
+                  Text("Pressure", style: title),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(wind, style: info),
+                  const SizedBox(height: 18),
+                  Text(pressure, style: info),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Humidity", style: title),
+                  const SizedBox(height: 18),
+                  Text("Feels Like", style: title),
+                ],
+              ),
+               Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(humidity, style: info),
+                  const SizedBox(height: 18),
+                  Text(feelslike, style: info),
+                ],
+              ),
+            ])
       ],
     ),
   );
