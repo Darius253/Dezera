f1f317112f173ef698bcfd4156d45738b02296c9
Darius <68332672+Darius253@users.noreply.github.com>
2022-02-11 23:20:53 +0000

    added Weather api

---
+++ dezera/lib/services/weather_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:dezera/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherApiCLient {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c307c1791102696e2aaf38f84a5bdbd1&units=metric");
+
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    print(Weather.fromJson(body));
+    return Weather.fromJson(body);
+  }
+}
